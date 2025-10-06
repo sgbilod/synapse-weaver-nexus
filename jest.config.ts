fc44074caafd45840c34fe599e116e25b7fe2d4e
sgbilod <sgbilod@users.noreply.github.com>
2025-10-05 23:11:59 -0400

    fix: standardize formatting in CI and configuration files

--- jest.config.ts
+++ jest.config.ts
@@ -6,12 +6,15 @@ const config: Config = {
   roots: ["<rootDir>/packages", "<rootDir>/tests"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
-  collectCoverageFrom: ['packages/**/*.ts'],
-  coverageDirectory: '<rootDir>/coverage',
+  collectCoverageFrom: ["packages/**/*.ts"],
+  coverageDirectory: "<rootDir>/coverage",
   transform: {
-    '^.+\\.(t|j)sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.base.json' }]
+    "^.+\\.(t|j)sx?$": [
+      "ts-jest",
+      { tsconfig: "<rootDir>/tsconfig.base.json" },
+    ],
   },
-  testPathIgnorePatterns: ['<rootDir>/packages/ui-desktop/tests']
+  testPathIgnorePatterns: ["<rootDir>/packages/ui-desktop/tests"],
 };
 
 export default config;
