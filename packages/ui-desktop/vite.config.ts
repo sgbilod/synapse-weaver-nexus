7be946f0dcecab7004eb3691687c6d502c97ef9b
sgbilod <sgbilod@users.noreply.github.com>
2025-10-08 21:41:42 -0400

    refactor(ui): update preload file extension and improve code formatting in configuration

--- packages/ui-desktop/vite.config.ts
+++ packages/ui-desktop/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import electron from 'vite-plugin-electron';
-import renderer from 'vite-plugin-electron-renderer';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import electron from "vite-plugin-electron";
+import renderer from "vite-plugin-electron-renderer";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
@@ -9,11 +9,11 @@ export default defineConfig({
     electron([
       {
         // Main-Process entry file of the Electron App.
-        entry: 'src/main.ts',
+        entry: "src/main.ts",
         vite: {
           build: {
             rollupOptions: {
-              external: ['dockerode', 'ssh2'],
+              external: ["dockerode", "ssh2"],
             },
           },
         },
